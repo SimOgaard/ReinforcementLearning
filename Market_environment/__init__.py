4d177a6b07223b6f3a69d96a5928ed38780a531b
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-02-17 12:13:27 +0100

    Update __init__.py

--- Market_environment/__init__.py
+++ Market_environment/__init__.py
@@ -1,15 +1,16 @@
 from gym.envs.registration import register
 from copy import deepcopy
+from datasets.utils import load_dataset as _load_dataset
 
-from . import datasets
-
+df = _load_dataset('A', 'Date')
 
 register(
+    # DF = _load_dataset('A', 'Date')
     id='market-v0',
     entry_point='Market_environment.envs:MarketEnv',
     kwargs={
-        'df': deepcopy(datasets.A),
+        'df': deepcopy(datasets.df),
         'window_size': 30,
-        'frame_bound': (30, len(datasets.A))
+        'frame_bound': (30, len(datasets.df))
     }
 )
