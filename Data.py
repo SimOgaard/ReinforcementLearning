db29eedf0f247676a337b3c69226bd6578582d42
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-03-09 08:35:26 +0100

    changes'

--- Data.py
+++ Data.py
@@ -3,13 +3,9 @@ from datetime import timedelta, date
 def FileSortments(startDate):
     import requests
     import pandas_datareader.data as web
-    import os
     import pickle
     import datetime as dt
     import bs4 as bs
-
-    if not os.path.exists("csv"):
-        os.makedirs("csv")
 
     table = bs.BeautifulSoup(requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies").text, "lxml").find("table", {"class":"wikitable sortable"})
     tickers = []
@@ -30,4 +26,4 @@ def FileSortments(startDate):
 startDate = date(2017, 1, 1)
 endDate = date(2020, 2, 1)
 
-FileSortments(startDate) # Takes A While
+FileSortments(startDate)
