ab2f91ae2221ef4ec57161655885c8300919aa63
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-03-09 08:58:57 +0100

    uh

--- Data.py
+++ Data.py
@@ -6,6 +6,12 @@ def FileSortments(startDate):
     import pickle
     import datetime as dt
     import bs4 as bs
+    import os
+    import argparse
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--stockAmount", required=False, default=500)
+    stockAmount = int(args["stockAmount"])
 
     table = bs.BeautifulSoup(requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies").text, "lxml").find("table", {"class":"wikitable sortable"})
     tickers = []
@@ -17,7 +23,9 @@ def FileSortments(startDate):
     with open("/content/Market-environment/Market_environment/datasets/sp500tickers.pickle","wb") as f:
         pickle.dump(tickers,f)
 
-    for ticker in tickers:
+    os.mkdir("/content/Market-environment/Market_environment/datasets/data/")
+
+    for ticker in tickers[:stockAmount]:
         try:
             web.DataReader(ticker, "yahoo", dt.datetime(startDate.year, startDate.month, startDate.day), dt.datetime.now()).to_csv("/content/Market-environment/Market_environment/datasets/data/{}.csv".format(ticker))
         except Exception as e:
