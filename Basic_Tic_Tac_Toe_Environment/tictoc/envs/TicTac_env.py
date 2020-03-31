bd8874dff190d5b48c3ca87682ffacc79c0aa4f5
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-03-31 13:38:25 +0200

    Update TicTac_env.py

--- Basic_Tic_Tac_Toe_Environment/tictoc/envs/TicTac_env.py
+++ Basic_Tic_Tac_Toe_Environment/tictoc/envs/TicTac_env.py
@@ -17,32 +17,35 @@ class TicTac(gym.Env):
                 return 1
             if x == [2,2,2]:
                 return 2
-        rotated = list(zip(*reversed(self.state)))
-        for y in rotated:
+
+        for y in list(zip(*reversed(self.state))):
             if y == [1,1,1]:
                 return 1
             if y == [2,2,2]:
                 return 2
+
         if self.state[0][2] == self.state[1][1] == self.state[2][0] == 1:
             return 1
         if self.state[0][2] == self.state[1][1] == self.state[2][0] == 2:
             return 2
+
         return 0
 
     def step(self, target):
         if self.done:
             print("game over")
-        elif not self.state[int(target/3)][target%3]:
+        elif self.state[int(target/3)][target%3]:
+            print("invalid step")            
+        else:
+            print(self.turn)
             if not self.turn%2 == 0:
                 self.state[int(target/3)][target%3] = 1
             else:
                 self.state[int(target/3)][target%3] = 2
             self.turn += 1
             if self.turn == 9:
-                self.done == True
-            self.render()
-        else:
-            print("invalid step")
+                print(self.turn)
+                self.done = True
 
         winner = self.check()
         if winner:
@@ -53,15 +56,24 @@ class TicTac(gym.Env):
             else:
                 self.reward = -1
 
-        return [self.state, self.reward, self.done]
+        return [self.state, self.reward, self.done, self.turn]
 
     def reset(self):
         self.__init__()
+        return self.state
 
     def render(self):
-        # render = self.state
-        # render[render = 0] = " "
-        # render[render = 1] = "x"
-        # render[render = 2] = "o"
-        # print(render)
-        print(self.state)
+        # # render = self.state
+        # # render[render = 0] = " "
+        # # render[render = 1] = "x"
+        # # render[render = 2] = "o"
+        # # print(render)
+        # for x in self.state:
+        #     print(x)
+        #     print("")
+        # # print(self.state)
+
+        for i in range(3):
+			for j in range(3):
+				print(self.state[i][j], end = " ")
+			print("")
