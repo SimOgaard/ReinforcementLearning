ff7153ebc2b51c5f3ac12ebe915858c92bae3d86
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-03-31 21:12:16 +0200

    wopsie

--- Market_Environment/MarketEnvironmentBasic/__init__.py
+++ Market_Environment/MarketEnvironmentBasic/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='Market-v0',
-    entry_point='MarketEnvironmentBasic.envs:Market',
+    entry_point='MarketEnvironmentBasic.envs:Market_Basic',
 )
--- Market_Environment/MarketEnvironmentMedium/__init__.py
+++ Market_Environment/MarketEnvironmentMedium/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='Market-v1',
-    entry_point='MarketEnvironmentMedium.envs:Market',
+    entry_point='MarketEnvironmentMedium.envs:Market_Medium',
 )
