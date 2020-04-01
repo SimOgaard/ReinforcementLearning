277782874fa45c61a84ec009a4cd9feab481c864
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-01 11:04:53 +0200

    Update Market_Basic_env.py

--- Market_Environment/MarketEnvironmentBasic/envs/Market_Basic_env.py
+++ Market_Environment/MarketEnvironmentBasic/envs/Market_Basic_env.py
@@ -2,12 +2,14 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class Market_Basic(gym.Env):
     metadata = {'render.modes': ['human']}
     
     def __init__(self, df):
         self.df = df
+        self.df.append(pd.Series(name="Guess"))
         self.state_index = 0
         self.reward = 0
         self.last_value = 0
@@ -17,11 +19,12 @@ class Market_Basic(gym.Env):
 
         if self.last_value <= self.df.loc[self.state_index, "Open"] and target == 0 or self.last_value > self.df.loc[self.state_index, "Open"] and target == 1:
             self.reward = 1
+            self.pd.loc[self.state_index,"Guess"] = 1
         else:
             self.reward = -1
-    
+            self.pd.loc[self.state_index,"Guess"] = 0
+        
         self.last_value = self.df.loc[self.state_index, "Open"]
-        # self.render()
         self.state_index += 1
         if pd.Index(self.df["Open"]).size == self.state_index:
             self.done = True
@@ -35,4 +38,4 @@ class Market_Basic(gym.Env):
         self.last_value = 0
 
     def render(self):
-        print(self.df.loc[self.state_index, "Open"])
+        self.df.plot(kind='scatter',x='index',y='value',color='red')
