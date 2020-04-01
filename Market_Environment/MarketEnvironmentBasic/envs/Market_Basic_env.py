de7fd368aba47819eaa4dc890708404c43c3b748
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-01 12:47:28 +0200

    better looking

--- Market_Environment/MarketEnvironmentBasic/envs/Market_Basic_env.py
+++ Market_Environment/MarketEnvironmentBasic/envs/Market_Basic_env.py
@@ -9,24 +9,29 @@ class Market_Basic(gym.Env):
     
     def __init__(self, df):
         self.df = df
-        self.df.append(pd.Series(name="Guess"))
+        # self.df.append(pd.Series(name="Guess"))
+        self.max_index = pd.Index(self.df["Open"]).size
         self.state_index = 0
         self.reward = 0
         self.last_value = 0
         self.done = False
+        self.df[["Close"]].plot()
 
     def step(self, target):
 
-        if self.last_value <= self.df.loc[self.state_index, "Open"] and target == 0 or self.last_value > self.df.loc[self.state_index, "Open"] and target == 1:
+        self.this_value = self.df.loc[self.state_index, "Open"]
+
+        if self.last_value <= self.this_value and target == 0 or self.last_value > self.this_value and target == 1:
             self.reward = 1
-            self.df.loc[self.state_index,"Guess"] = 1
+            # self.df.loc[self.state_index,"Guess"] = 1
         else:
             self.reward = -1
-            self.df.loc[self.state_index,"Guess"] = 0
-        
-        self.last_value = self.df.loc[self.state_index, "Open"]
+            # self.df.loc[self.state_index,"Guess"] = 0
+        plt.plot(self.state_index, self.this_value, marker=".", color='green' if self.reward==1 else "red")
+
+        self.last_value = self.this_value
         self.state_index += 1
-        if pd.Index(self.df["Open"]).size == self.state_index:
+        if self.max_index == self.state_index:
             self.done = True
 
         return [self.state_index, self.reward, self.done]
@@ -36,14 +41,11 @@ class Market_Basic(gym.Env):
         self.state_index = 0
         self.reward = 0
         self.last_value = 0
-
-    def render(self):
-
         self.df[["Close"]].plot()
 
-        for row in range(pd.Index(self.df["Open"]).size):
-            plt.plot(row, self.df.loc[row,"Open"], marker=".", color='green' if self.df.loc[row,"Guess"] else "red")
-
+    def render(self):
+        # for row in range(pd.Index(self.df["Open"]).size):
+        #     plt.plot(row, self.df.loc[row,"Open"], marker=".", color='green' if self.df.loc[row,"Guess"] else "red")
         plt.show()
 
 
