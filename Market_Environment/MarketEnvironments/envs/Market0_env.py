af7e8de35e809a476cca895dae70d13280f86a6c
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-06 21:14:42 +0200

    this is right i thijnl

--- Market_Environment/MarketEnvironments/envs/Market0_env.py
+++ Market_Environment/MarketEnvironments/envs/Market0_env.py
@@ -9,19 +9,18 @@ class Market0(gym.Env):
     
     def __init__(self, df):
         self.prices = df.loc[:, 'Close'].to_numpy()
-        self.max_index = self.prices.size
+        self.max_index = self.prices.size-1
         self.selection = []
         
         self.state_index = 0
-        self.last_value = 0
         self.done = False
         
     def step(self, target):
         self.this_value = self.prices[self.state_index]
+        self.next_value = self.prices[self.state_index+1]
 
         self.reward = self.get_reward(target)
     
-        self.last_value = self.this_value
         self.state_index += 1
         
         self.done = self.max_index == self.state_index
@@ -29,7 +28,7 @@ class Market0(gym.Env):
         return [self.state_index, self.reward, self.done]
 
     def get_reward(self, target):
-        if self.last_value <= self.this_value and target == 0 or self.last_value > self.this_value and target == 1:
+        if self.this_value <= self.next_value and target == 0 or self.this_value > self.next_value and target == 1:
             self.selection.append("green")
             return 1
         self.selection.append("red")
@@ -38,7 +37,6 @@ class Market0(gym.Env):
     def reset(self):
         self.done = False
         self.state_index = 0
-        self.last_value = 0
         self.selection = []
         return self.state_index
 
