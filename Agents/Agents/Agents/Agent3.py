5fff9737b21ea5ee0b1d905767dc470ee5122aa5
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-09 18:19:07 +0200

    Update Agent3.py

--- Agents/Agents/Agents/Agent3.py
+++ Agents/Agents/Agents/Agent3.py
@@ -9,7 +9,7 @@ from collections import deque
 from gym import spaces
 
 class Agent3:
-    def __init__(self, df, init_invest = 20000):
+    def __init__(self, df):
         self.action_space = spaces.Discrete(3)
 
         print(self.action_space)
@@ -19,7 +19,7 @@ class Agent3:
         print(self.stock_price_history)
 
         stock_max_price = self.stock_price_history.max(axis=1)
-        price_range = [[0, mx] for mx in stock_max_price]
+        price_range = [[1, mx] for mx in stock_max_price]
 
         print(stock_max_price)
         print(price_range)
