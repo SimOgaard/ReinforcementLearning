d17f6bffb3420bdd25856b4e68c1b126f2617c6b
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-09 22:09:31 +0200

    Update Agent2.py

--- Agents/Agents/Agents/Agent2.py
+++ Agents/Agents/Agents/Agent2.py
@@ -9,7 +9,7 @@ from gym import spaces
 
 import math
 
-from collections import deque
+# from collections import deque
 
 class Agent2:
     
@@ -38,7 +38,7 @@ class Agent2:
         self.model = self.mlp()
 
 
-        self.memory = deque(maxlen=1000)
+        # self.memory = deque(maxlen=1000)
 
     def mlp(self):
         model = Sequential()
@@ -62,11 +62,16 @@ class Agent2:
         return 1 / (1 + math.exp(-x))
 
     def getState(self, data, t, n):
+        print("l")
         d = t - n + 1
+        print("a")
         block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
+        print("m")
         res = []
         for i in range(n - 1):
+            print("a")
             res.append(self.sigmoid(block[i + 1] - block[i]))
+        print("o")
 
         return np.array([res])
 
@@ -99,20 +104,20 @@ class Agent2:
 
 
 
-    def expReplay(self, batch_size):
-        mini_batch = []
-        l = len(self.memory)
-        for i in range(l - batch_size + 1, l):
-            mini_batch.append(self.memory[i])
+    # def expReplay(self, batch_size):
+    #     mini_batch = []
+    #     l = len(self.memory)
+    #     for i in range(l - batch_size + 1, l):
+    #         mini_batch.append(self.memory[i])
         
-        for state, action, reward, next_state, done in mini_batch:
-            target = reward
-            if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+    #     for state, action, reward, next_state, done in mini_batch:
+    #         target = reward
+    #         if not done:
+    #             target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
             
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+    #         target_f = self.model.predict(state)
+    #         target_f[0][action] = target
+    #         self.model.fit(state, target_f, epochs=1, verbose=0)
         
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay 
+    #     if self.epsilon > self.epsilon_min:
+    #         self.epsilon *= self.epsilon_decay 
