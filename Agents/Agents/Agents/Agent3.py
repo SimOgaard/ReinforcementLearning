d2a6929ba0502d62c309195e366c011eb3421619
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-09 17:36:06 +0200

    Update Agent3.py

--- Agents/Agents/Agents/Agent3.py
+++ Agents/Agents/Agents/Agent3.py
@@ -15,8 +15,8 @@ class Agent3:
         print(self.action_space)
 
         self.stock_price_history = np.around(df)
-        self.stock_price_history.drop(["Volume"], axis=1)
-        # self.stock_price_history.drop(4)
+        # self.stock_price_history.drop(["Volume"], axis=1)
+        self.stock_price_history.drop(4)
 
         print(self.stock_price_history)
 
