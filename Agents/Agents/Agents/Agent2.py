9647f94bf0b730fc791351d178895998a6a0fffb
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-09 21:30:36 +0200

    Update Agent2.py

--- Agents/Agents/Agents/Agent2.py
+++ Agents/Agents/Agents/Agent2.py
@@ -28,26 +28,52 @@ class Agent2:
         self.epsilon = 1
         self.epsilon_decay = 0.8
 
+        self.data = getStockDataVec("stock_name")
+
         self.model = self.mlp()
 
     def mlp(self):
         model = Sequential()
-        model.add(Dense(units=64, input_dim=self.observation_space, activation="relu"))
+        model.add(Dense(units=64, input_dim=100, activation="relu"))
         model.add(Dense(units=32, activation="relu"))
         model.add(Dense(units=8, activation="relu"))
         model.add(Dense(self.action_size, activation="linear"))
         model.compile(loss="mse", optimizer=Adam(lr=0.001))
         return model
 
+    def getStockDataVec(key):
+        vec = []
+        lines = open("/content/ReinforcementLearning/DataMarket/data/ABT.csv", "r").read().splitlines()
+
+        for line in lines[1:]:
+            vec.append(float(line.split(",")[4]))
+
+        return vec
+
+    def sigmoid(x):
+        return 1 / (1 + math.exp(-x))
+
+    def getState(self, data, t, n):
+        d = t - n + 1
+        block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
+        res = []
+        for i in range(n - 1):
+            res.append(sigmoid(block[i + 1] - block[i]))
+
+        return np.array([res])
+
     def act(self, state):
+
+        self.state = getState(self.data, state, self.state_size)
+
         if np.random.rand() < self.epsilon:
             self.random_action += 1
             return np.random.randint(self.action_size)
         else:
             self.mlp_action += 1
-            print(state)
-            print(self.model.predict(state))
-            return np.argmax(self.model.predict(state)[0])
+            # print(state)
+            print(self.model.predict(self.state))
+            return np.argmax(self.model.predict(self.state)[0])
 
     def new_episode(self):
         self.random_action = 0
