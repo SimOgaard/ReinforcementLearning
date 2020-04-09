26cc62ab4e112edaf7574fd10cbb81ffc1cea33d
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-09 21:56:48 +0200

    Update Agent2.py

--- Agents/Agents/Agents/Agent2.py
+++ Agents/Agents/Agents/Agent2.py
@@ -9,6 +9,8 @@ from gym import spaces
 
 import math
 
+from collections import deque
+
 class Agent2:
     
     def __init__(self, state_size, action_size, df):
@@ -34,6 +36,9 @@ class Agent2:
         self.data = self.getStockDataVec("stock_name")
 
         self.model = self.mlp()
+
+
+        self.memory = deque(maxlen=1000)
 
     def mlp(self):
         model = Sequential()
@@ -90,3 +95,24 @@ class Agent2:
     def action_sample(self):
         self.random_action += 1
         return np.random.randint(self.action_size)
+
+
+
+
+	def expReplay(self, batch_size):
+		mini_batch = []
+		l = len(self.memory)
+		for i in range(l - batch_size + 1, l):
+			mini_batch.append(self.memory[i])
+
+		for state, action, reward, next_state, done in mini_batch:
+			target = reward
+			if not done:
+				target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+
+			target_f = self.model.predict(state)
+			target_f[0][action] = target
+			self.model.fit(state, target_f, epochs=1, verbose=0)
+
+		if self.epsilon > self.epsilon_min:
+			self.epsilon *= self.epsilon_decay 
