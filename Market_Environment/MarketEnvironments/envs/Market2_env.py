f704418807effe19764f7d99575b3f3611b531d2
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2020-04-11 15:05:44 +0200

    Spring Cleaning

--- Market_Environment/MarketEnvironments/envs/Market2_env.py
+++ Market_Environment/MarketEnvironments/envs/Market2_env.py
@@ -54,6 +54,7 @@ class Market2(gym.Env):
         for column in self.prices.T:
             print(column)
             block = column[d:t + 1] if d >= 0 else -d * [column[0]] + column[0:t + 1]
+            print(block)
             res = []
             for i in range(n-1):
                 res.append(self.sigmoid(block[i + 1] - block[i]))
